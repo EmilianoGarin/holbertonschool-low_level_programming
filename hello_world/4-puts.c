cc7551155208eeb82fe0347e622bccc531809eab
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-15 08:34:39 -0800

    x

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-	char tex[] = "\"Programming is like building a multilingual puzzle\n";
+	char tex[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(tex);
 	return (0);
 }
