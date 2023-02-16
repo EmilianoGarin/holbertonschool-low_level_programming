9fee6608724af912c345d98f82b3b1f6be0ce069
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-16 06:06:54 -0800

    x

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+
+	for (i = 65; i < 91; i++)
+		putchar(i);
+
+	putchar(10);
+
+	return (0);
+}
