e731bf5737a085b2a4e2614d61ed25bbac93f7d4
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-16 06:27:29 -0800

    x

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+
+	return (0);
+}
