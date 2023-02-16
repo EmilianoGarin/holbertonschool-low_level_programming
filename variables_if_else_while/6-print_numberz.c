69a87e0053859df73eac6d7db9bddbb9ecc63e6d
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-16 07:07:56 -0800

    x

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	putchar(10);
+
+	return (0);
+}
