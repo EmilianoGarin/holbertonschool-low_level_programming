4e8d8485f163e50645d25f38a0efd026fca2bdbc
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-16 07:24:13 -0800

    x

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i < 96; i--)
+		putchar(i);
+
+	putchar(10);
+
+	return (0);
+}
