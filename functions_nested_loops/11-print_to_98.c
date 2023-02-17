9d7b795faee07da9b2fc68286ec3f211ae229c24
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-17 10:51:21 -0800

    x

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,8 @@
+#include <stdio.h>
 /**
- * _islower - receive a int
+ * print_to_98 - receive a int
  *
- *n@: decimal
+ * @n: decimal
  *
  * Description: namber to 98
  *
@@ -9,9 +10,12 @@
  */
 void print_to_98(int n)
 {
+	printf("%d", n);
 	if (n > 98)
-	{
-		for (; n > 97; n--)
+		for (n = n - 1; n > 97; n--)
 			printf(", %d", n);
+	if (n < 98)
+		for (n = n + 1; n < 99; n++)
+			printf(", %d", n);
+	printf("\n");
 }
-
--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_last_digit - receive a int
  *
