9d3dc2cf7c13230767712049ddc367c5cf6e0089
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-17 09:44:46 -0800

    x

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,7 +13,10 @@ void times_table(void)
 	for (x = 0; x < 10; x++)
 		for (y = 0; y < 10; y++)
 		{
-			printf("% 3d", x * y);
+			if (y == 0)
+				printf("0");
+			else
+				printf("% 3d", x * y);
 			if (y == 9)
 				printf("\n");
 			else
