c87be209a71076775875a6fdc52403d4380d01cd
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-17 10:30:30 -0800

    x

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+/**
+ * _islower - receive a int
+ *
+ *n@: decimal
+ *
+ * Description: namber to 98
+ *
+ * Return:  0 is good
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 97; n--)
+			printf(", %d", n);
+}
+
