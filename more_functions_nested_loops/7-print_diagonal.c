891bf913d08333a34ab829618819acaf63dda81f
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-22 11:18:37 -0800

    x

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_numbers - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_diagonal(int n)
+{
+	int i, x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i < x; i++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar(10);
+		}
+	}	
+	_putchar(10);
+}
