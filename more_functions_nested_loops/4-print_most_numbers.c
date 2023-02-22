4576f00e2e70f3332b08c9437a8e7b4b920bd434
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-22 10:08:27 -0800

    x

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		if ((i != 50) || (i != 52))
+			_putchar(i);
+
+	_putchar(10);
+}
