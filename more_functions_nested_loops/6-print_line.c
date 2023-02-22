a3f441050924d0af83cf286a5bfeceee9b629ba7
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-22 10:33:16 -0800

    x

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - empty point
+ *
+ *@n: number
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+		n = 0;
+	for (i = 0; i == n; i++)
+		_putchar('_');
+	_putchar(10);
+}
