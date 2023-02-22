73212f569ae1317c5991b9316dd8072d69b6bfed
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-22 09:34:36 -0800

    x

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+ * mul - receive a int
+ *
+ *@a: decimal
+ *
+ *@b: decimal
+ *
+ * Description: multiplies
+ *
+ * Return:  0 if all is good
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
