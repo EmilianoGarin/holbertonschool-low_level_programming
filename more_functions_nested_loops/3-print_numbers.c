901f463728794700ee57f16af96d5053aebf7f2a
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-22 09:51:15 -0800

    x

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void);
+{
+int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+
+	_putchar(10);
+
+	return (0);
+}
