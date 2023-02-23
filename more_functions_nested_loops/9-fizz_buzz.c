76fa11e63b7bd7807f9cca16cbce5a110fbd2a39
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-23 04:54:03 -0800

    x

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: FizzBuzz
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 101; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		if ((i % 5) == 0)
+			printf("Buzz");
+		else if ((i % 3) != 0)
+			printf("%d", i);
+		printf(" ");
+	}
+
+	return (0);
+}
