8016e297e72e8bf153c3c3101595ccc8f907ad65
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-23 06:09:30 -0800

    x

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - empty point
+ *
+ *@size: triangles
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_triangle(int size)
+{
+	int i, x;
+
+	if (size != 0)
+		for (x = 0; x < size; x++)
+		{
+			for (i = size; i > x - 1; i--)
+				_putchar(' ');
+			for (i = 0; i < x + 1; i++)
+				_putchar('#');
+			_putchar(10);
+		}
+	else
+		_putchar(10);
+}
