239c38e51491cd4f810ec188af91c112891f7ec8
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-27 13:28:25 -0800

    x

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcpy - *dest *src
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ *
+ * description: copy
+ * 
+ * return: char pointer of dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+	char *ret;
+
+	ret = dest;
+	
+	while (*(src + x) != '\0')
+	{
+		*(dest + x) = *(src + x);
+		x++;
+	}
+	return (ret);
+}
