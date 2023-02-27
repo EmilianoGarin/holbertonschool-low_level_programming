987284d85e3bd47eea66a77de26f7fffbc0b76ca
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-27 10:21:03 -0800

    x

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - empty point
+ *
+ * @str: char pointer
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+void puts_half(char *str)
+{
+	int x = 0;
+	int i;
+
+	while (*(str + x) != '\0')
+		x++;
+	if (x % 2 != 0)
+		i = (x - 1) / 2;
+	else
+		i = x / 2;
+	for (; i <= x; i++)
+		_putchar(*(str + i));
+	_putchar(10);
+}
