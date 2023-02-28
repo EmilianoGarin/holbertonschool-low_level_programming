85b0243141b7927b64076d692a855ebb4ea612c5
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-28 10:17:02 -0800

    x

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int x = 0;
 
 
-	while ((*(dest + x) != '\0') && (x != n))
+	while ((*(dest + x) != '\0') && (*(src + x) != '\0') && (x != n))
 	{
 		*(dest + x) = *(src + x);
 		x++;
