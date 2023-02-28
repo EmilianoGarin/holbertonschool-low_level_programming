4f6456d234daa8e3099966ee2f3cf6c6f0e53b43
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-28 10:13:07 -0800

    x

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - empty point
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: int
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+
+	while ((*(dest + x) != '\0') && (x != n))
+	{
+		*(dest + x) = *(src + x);
+		x++;
+	}
+	return (dest);
+}
