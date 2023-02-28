e23b6f93f54070ed867ca0f6e7aadeab2a0d5416
EmilianoGarin <emiliano.garin333@gmail.com>
2023-02-28 07:10:38 -0800

    x

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,27 +16,29 @@ int _atoi(char *s)
 	int cont = 0;
 	int ret = 0;
 	int pot = 1;
-	int sep[];
-	char sig = NULL;
+	int sep[20];
+	char sig = ' ';
 
 	while (*(s + x) != '\0')
 	{
 		if ((*(s + x) > 47) && (*(s + x) < 59))
-			{
-			if ((sig == NULL) && (x > 0) && (*(s + x - 1) == '-'))
+		{
+			if ((sig == ' ') && (x > 0) && (*(s + x - 1) == '-'))
 					sig = '-';
 			sep[cont] = (*(s + x) - 48);
 			cont++;
-			}
+			if (!((s[x + 1] > 47) && (s[x + 1] < 59)))
+				break;
+		}
 		x++;
 	}
 
 	for (x = cont - 1; x > -1; x--)
 		{
-		ret = (sep[x] * pot);
+		ret = ret + (sep[x] * pot);
 		pot = pot * 10;
 		}
-	if (sig != NULL)
+	if (sig != ' ')
 		ret = ret * -1;
 	return (ret);
 }
