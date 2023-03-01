fd568c76fea4fa4a810f6c26085d41e62de4d213
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-01 07:49:11 -0800

    x

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * string_toupper - de minuscula a mayuscula
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - todas las plaabras comienzan con mayuscula
+ *
+ * @s: string
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+char *cap_string(char *s)
+{
+	int i, x = 0;
+	char pre_pal[] = " 	\n,;.!\"(){}?";
+
+	while (s[x] != '\0')
+	{
+
+		for (i = 0; i < 13; i++)
+			if (s[x] == pre_pal[i])
+				if (s[x + 1]  > 96 && s[x + 1] < 123)
+					s[x + 1] = (s[x + 1] - 32);
+		x++;
+	}
+	return (s);
+}
