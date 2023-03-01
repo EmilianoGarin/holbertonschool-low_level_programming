768799ed6dd31b5d4e4dc0ad3ee8408154870ae5
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-01 10:04:21 -0800

    x

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - busca c en s
+ *
+ * @s: string
+ * @c: char
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x = 0;
+	char *p = NULL;
+
+	while (s[x] != '\0' && s[x] != c)
+		x++;
+
+	if (s[x] == '\0')
+	{
+		return (p);
+	}
+	else
+	{
+		p = &s[x];
+		return (p);
+	}
+}
