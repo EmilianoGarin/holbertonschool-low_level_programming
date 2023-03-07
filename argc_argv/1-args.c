036d0f02f89c6b03a1010687735b946189f7bbe8
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-07 09:52:17 -0800

    x

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 0)
+		printf("%s\n", argv[0]);
+	printf("%d\n", argc);
+	return (0);
+}
