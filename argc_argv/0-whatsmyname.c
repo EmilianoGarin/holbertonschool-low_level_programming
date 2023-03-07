fea8b50175dddc435cf026c4c40f9a933f61bc1d
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-07 09:23:51 -0800

    x

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - empty point
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char  *argv[])
+{
+	int i = argc;
+	
+	i = 0;
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
