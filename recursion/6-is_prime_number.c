ef2db9e4e1e982a76db9a6d9ce8fe262b0832280
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-07 07:37:10 -0800

    x

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - de minuscula a mayuscula
+ *
+ * @s: string
+ *
+ * Description: size
+ *
+ * Return: Always 0 (success)
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0 || n == 1)
+		return (0);
+	else if ((n % n) % 1 == 0)
+		return (1);
+	else
+		return (1);	
+}
