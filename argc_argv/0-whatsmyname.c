1619254731850f18a6c12a69e054bdc80144820e
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-07 09:27:24 -0800

    x

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - empty point
+ * main - recibe su nombre en la pocicion 0 de argv
  *
- * Description: size
+ * @argc: longitud de argv
+ * @argv: lista de los aracteres que acompañan al programa
+ *
+ * Description: imprime el nombre del programa sacado de los argumentos
  *
  * Return: Always 0 (success)
  */
@@ -11,7 +14,7 @@
 int main(int argc, char  *argv[])
 {
 	int i = argc;
-	
+
 	i = 0;
 	printf("%s\n", argv[i]);
 
