b46d13ed4ea367e4d92dc6d647a23361574fc9f7
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-13 12:01:21 -0700

    x

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,7 +5,7 @@
  * free_grid - libera la memoria pedida con malloc en grid
  *
  * @grid: matriz de 2 dimenciones
- * @height: altura de la matriz 
+ * @height: altura de la matriz
  *
  * Description: libera cada malloc que forman las filas y por
  * ultimo el arreglo que las contenia
