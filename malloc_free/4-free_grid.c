523ede8bf7a5083a0a839b27e1091e40f0eb428e
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-13 12:00:31 -0700

    x

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - libera la memoria pedida con malloc en grid
+ *
+ * @grid: matriz de 2 dimenciones
+ * @height: altura de la matriz 
+ *
+ * Description: libera cada malloc que forman las filas y por
+ * ultimo el arreglo que las contenia
+ *
+ * Return: no retorna nada
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+		free(grid[x]);
+	free(grid);
+}
