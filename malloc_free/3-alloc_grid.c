5bade57f0f1194d1522d8ee14882d3a112dde4e2
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-13 11:43:18 -0700

    x

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - crea un arreglo de 2 dimenciones con malloc
+ *
+ * @width: width
+ * @height: height
+ *
+ * Description:crea un arreglo inicialisado con 0 de height de
+ * alto y width de ancho
+ *
+ * Return: el puntero doble que apunta a la pocicion 0, 0
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int x, i;
+	int **ret = NULL;
+
+	if (width <= 0 || height <= 0)
+		return (ret);
+	ret = (int **)malloc(sizeof(int *) * height);
+	if (ret == NULL)
+		return (NULL);
+	for (x = 0; x < height; x++)
+	{
+		ret[x] = (int *)malloc(sizeof(int) * width);
+		if (ret[x] == NULL)
+			return (NULL);
+		for (i = 0; i < width; i++)
+			ret[x][i] = 0;
+	}
+	return (ret);
+}
