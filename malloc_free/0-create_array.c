0cb2c8594eeae8a6f664e3510ba74c7abfe15b58
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-13 05:54:38 -0700

    x

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -21,7 +21,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size != 0)
 	{
-		ret = (char*)malloc(size);
+		ret = (char *)malloc(size);
 		aux = ret;
 		for (i = 0; i < size; i++)
 			aux[i] = c;
