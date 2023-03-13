85bc684278d307970c7be8ed094ae7064ff84e60
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-13 05:45:17 -0700

    x

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - hace un arreglo dinamico
+ *
+ * @c: char
+ * @size: tamaño del arreglo
+ *
+ * Description: crea un arreglo y lo llena con el caracter c
+ *
+ * Return: el puntero que apunta al pimer espacio reservado con
+ * malloc
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *aux;
+	char *ret = NULL;
+	unsigned int i;
+
+	if (size != 0)
+	{
+		ret = (char *)malloc(size);
+		aux = ret;
+		for (i = 0; i < size; i++)
+			aux[i] = c;
+	}
+	return (ret);
+}
