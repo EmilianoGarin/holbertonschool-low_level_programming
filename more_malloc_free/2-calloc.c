19c5004a3e084b16f6751dae26affe90e279152b
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-14 11:08:48 -0700

    x

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - x
+ *
+ * @nmemb: numero de espacios de malloc
+ * @size: numero de bytes que ocupa el tipo de dato
+ *
+ * Description: crea un malloc con nmemb espacios
+ *
+ * Return: el puntero al malloc
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ret = NULL;
+
+	if (nmemb == 0 || size == 0)
+		return (ret);
+	ret = malloc (size * nmemb);
+	if (ret == NULL)
+		return (ret);
+	return(ret);
+}
