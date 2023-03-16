1f713d391fe2abcf9226d1c62451bdf887b4a812
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-16 10:11:25 -0700

    x

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - empty
+ *
+ * Description: imprime el nombre del programa
+ *
+ * Return: 0 si all right 
+ */
+
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
