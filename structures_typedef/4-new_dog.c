687e4bb0ea43098dd6f2084ebfb8eaa1323bf117
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-20 14:26:56 -0700

    x

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,13 +19,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *k9;
 
 	k9 = malloc(sizeof(dog_t));
-	if (k9 != NULL)
-	{
-		(*k9).name = name;
-		(*k9).age = age;
-		(*k9).owner = owner;
-	}
-	else
+	if (k9 == NULL)
 		return (NULL);
+	(*k9).name = name;
+	(*k9).age = age;
+	(*k9).owner = owner;
 	return (k9);
 }
