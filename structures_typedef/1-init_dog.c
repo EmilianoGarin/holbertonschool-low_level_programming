8e9381f7dcb40cb555854687299825578c79fdc4
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-20 12:30:21 -0700

    x

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - inicializa una instancia de dog
+ *
+ * @d: struct dog
+ * @name: el nombre de la mascota (usar el . para acceder es lo mismo que ->)
+ * @age: la edad
+ * @owner: el nombre del dueño
+ *
+ * Description: carga los balores de name, age y owner en d
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	d->owner = owner;
+}
