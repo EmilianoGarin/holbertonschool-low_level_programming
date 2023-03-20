3e7b164181b8c237276da5577319a75fb1ea5657
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-20 14:40:25 -0700

    x

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - libera el malloc que guarda d
+ *
+ * @d: struct dog
+ *
+ * Description: libera el sepacio al que apunta d
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
