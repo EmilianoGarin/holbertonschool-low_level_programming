3c1d4e9fe00077cdb611c002d57fb29511e76a6c
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-23 09:32:15 -0700

    x

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_strings - imprime un a lista de palabras con un separador
+ *
+ * @separator: esla string que separa las palabras
+ * @n:se presupone que es la cantidad de elementos en ...
+ *
+ * Description:  imprime la lista de palabras con separator entre cada una
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	char *str = NULL;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(ap, n);
+	if (separator == NULL)
+	{
+		str = va_arg(ap, char*);
+		for (i = 0; i < n - 1; i++)
+			if (str == NULL)
+				printf("%s%s", "(nil)", "");
+			else
+				printf("%s%s", str, "");
+	}
+	else
+	{
+		str = va_arg(ap, char*);
+		for (i = 0; i < n - 1; i++)
+			if (str == NULL)
+				printf("%s%s", "(nil)", separator);
+			else
+				printf("%s%s", str, separator);
+	}
+	printf("%s\n", va_arg(ap, char*));
+	va_end(ap);
+}
