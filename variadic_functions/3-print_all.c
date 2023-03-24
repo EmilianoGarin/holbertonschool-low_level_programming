45e8155f7918d058f0c9305d1f6fb1e2dc3d610f
EmilianoGarin <emiliano.garin333@gmail.com>
2023-03-24 10:42:43 -0700

    x

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,13 +4,14 @@
 #include <stdio.h>
 
 /**
- * print_numbers - imprime un a lista de numeros con un separador
- * 
- * @separator: esla string que separa los numeros
- * @n:se presupone que es la cantidad de elementos en ...
- * 
- * Description:  imprime la lista de numeros con separator entre cada uno
- * 
+ *  print_all - imprime char, integer, float y string
+ *
+ *  @format: es un string que guarda las el tipo de vaiable ingresado
+ *
+ * Description: verfica que el valor pasado por format este dentro del formato
+ * previsto por la funcuion, si lo esta lo imprime,
+ * si no pasa al sigiente termino
+ *
  * Return: void
  */
 
@@ -25,38 +26,31 @@ void print_all(const char * const format, ...)
 	{
 		if (i != 0 && x == 1)
 			printf(", ");
-		switch (format[i]){
-			case'c':
+		switch (format[i])
+		{
+		case'c':
+			printf("%c", va_arg(ap, int));
+			x = 1;
+			break;
+		case'i':
+			printf("%d", va_arg(ap, int));
+			x = 1;
+			break;
+		case'f':
+			printf("%f", va_arg(ap, double));
+			x = 1;
+			break;
+		case's':
+			str =  va_arg(ap, char*);
+			x = 1;
+			if (str == NULL)
 			{
-				printf("%c", va_arg(ap, int));
-				x = 1;
+				printf("(nil)");
 				break;
 			}
-			case'i':
-			{
-				printf("%d", va_arg(ap, int));
-				x = 1;
-				break;
-			}
-			case'f':
-			{
-				printf("%f", va_arg(ap, double));
-				x = 1;
-				break;
-			}
-			case's':
-			{
-				str =  va_arg(ap, char*);
-				x = 1;
-				if (str == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
-				break;
-			}
-			default:
+			printf("%s", str);
+			break;
+		default:
 			x = 0;
 		};
 		i++;
