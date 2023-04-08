b2d75e74428aa1a01a27d9fa3c1b13f9ad80b6a6
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-08 16:47:41 -0700

    x

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -11,6 +11,8 @@ void free_list(list_t *head)
 {
 	if (head->next != NULL)
 		free_list(head->next);
-	free(head->str);
-	free(head->next);
+	if (head->str != NULL)
+		free(head->str);
+	if (head == NULL)
+		free(head);
 }
