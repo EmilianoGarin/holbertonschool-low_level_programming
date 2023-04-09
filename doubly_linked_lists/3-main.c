c782754cbe25d45e7dda440c6bdd90b86e73e18a
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-09 10:04:58 -0700

    x

--- doubly_linked_lists/3-main.c
+++ doubly_linked_lists/3-main.c
@@ -2,6 +2,26 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+size_t _print_dlistint_backward(const dlistint_t *h)
+		{
+					    const dlistint_t *current = h;
+					    		        size_t count = 0;
+
+											    if (h == NULL)
+												    				            return (0);
+
+											    			        while (current->next != NULL)
+																					        current = current->next;
+
+																			    while (current != NULL)
+																				    					        {
+																																		        printf("%d\n", current->n);
+																																											        current = current->prev;
+																																																					        count++;
+																																																																    }
+
+																			    				        return (count);
+																									}
 
 /**
  *  * main - check the code
@@ -22,5 +42,6 @@ int main(void)
 					    add_dnodeint_end(&head, 402);
 					        add_dnodeint_end(&head, 1024);
 						    print_dlistint(head);
+						    _print_dlistint_backward(head);
 						        return (EXIT_SUCCESS);
 }
---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * _free_dlistint - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head)
+		free_dlistint(head->next);
+	free(head);
+}
