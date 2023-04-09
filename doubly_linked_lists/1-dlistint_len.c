e573c10a4a4c9f75870e7ae9e7d638eba0bda803
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-08 17:43:46 -0700

    x

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * dlistint_len - Prints str and len of a Linked list
+ * @h: heder of a linked list
+ * Return: return the numbers of elements
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	size_t cont = 0;
+
+	while (1)
+	{
+		if (h == NULL)
+			return (cont);
+		cont++;
+		if (h->next == NULL)
+			return (cont);
+		h = h->next;
+	}
+}
