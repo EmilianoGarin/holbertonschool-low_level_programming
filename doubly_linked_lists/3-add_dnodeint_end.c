d9dc0e453666fb6d0c3f9e68a8781861ed29d01a
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-09 09:38:27 -0700

    x

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - add a element to a Linked list
+ * @head: memiry address of a next element of a linked list
+ * @n: int
+ * Return: return the  memiry address of an elements
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *ret = NULL;
+	dlistint_t *fin = *head;
+
+	ret = malloc(sizeof(dlistint_t));
+	if (ret == NULL)
+		return (NULL);
+	ret->n = n;
+	ret->next = NULL;
+	ret->prev = *head;
+
+	if (*head == NULL)
+		*head = ret;
+	else
+	{
+		while (fin->next != NULL)
+			fin = fin->next;
+		fin->next = ret;
+	}
+
+	return (ret);
+}
