bbe30ac2c6d7a7def3a75d8d502b1c6f37d8a7c1
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-10 13:03:48 -0700

    x

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_binary - comber a decimal to binary and print them
+ * @n: unsigned long int
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mul = 0;
+	int cos = 0;
+
+	for (; (mul + 2) <= n; mul += 2)
+		cos++;
+	if (cos > 0)
+	{
+		print_binary(cos);
+		_putchar((n - mul) + '0');
+	}
+	else
+		_putchar(n + '0');
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef _TASK_
 #define _TASK_
 #include <stddef.h>
+#include <unistd.h>
+void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char);
 #endif
