50f4b840aa1deaefbf6d1b06a695455053e90213
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-10 14:02:55 -0700

    x

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,12 +12,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int i;
 
 	for (i = 0; i < index; i++)
-	{
-		if (n == 0)
-			return (-1);
 		n /= 2;
-	}
-	if (n == 0)
-		return (-1);
 	return (n % 2);
 }
