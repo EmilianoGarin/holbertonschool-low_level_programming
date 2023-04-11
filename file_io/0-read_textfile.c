3baf73311e986dab3b045dea80e668b1eaff4b54
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-11 11:17:21 -0700

    x

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fl, re;
+	int fl, re, wr;
 	char * txt;
 
 	fl = open(filename, O_RDONLY);/*O_RDONLY=1*/
@@ -18,10 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	re = read(fl, txt, letters);
 	if (re == -1)
 		return (0);
-	re = write(1, txt, re);
-	if (re == -1)
+	wr = write(1, txt, re);
+	if (wr == -1)
 		return (0);
 	free(txt);
 	close(fl);
-	return (re);
+	return (wr);
 }
