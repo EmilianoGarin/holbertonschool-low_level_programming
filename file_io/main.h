3f6a425b802ec4297454655367ff5bd1f3aa3f66
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-11 10:58:39 -0700

    x

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef _TASK_
 #define _TASK_
+#include <stddef.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char);
