1796e03d98b9c319d815496901eb94415de065ef
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-12 09:50:49 -0700

    x

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - crea un archivo
+ * @filename: char nombre del archivo
+ * @text_content: texto a cargar en el erchivo
+ * Return: retorna -1 si falla, retorna 1 si esta bien
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fl, wr = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fl = open(filename, O_WRONLY | O_APPEND);
+	if (fl == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[wr] != '\0')
+			wr++;
+		wr = write(fl, text_content, wr);
+		if (wr == -1)
+			return (-1);
+	}
+	close(fl);
+	return (1);
+}
