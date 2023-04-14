13016c7ee86665be2a975a0e1c5e2c5e620eab4a
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-14 14:22:34 -0700

    x

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - livera la memoria usada en la hash table
+ * @ht: es la direccion de la hash table
+ * Return: nada
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *nex = NULL, *aux = NULL;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		aux = ht->array[i];
+		while (aux != NULL)
+		{
+			nex = aux->next;
+			free(aux);
+			aux = nex;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
