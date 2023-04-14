feba2bed686b78880fb30f1d05b458b86a1191c9
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-14 14:03:58 -0700

    x

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int x, i = 0;
+	unsigned long int x = 0, i = 0;
 	hash_node_t *aux = NULL;
 
 	if (ht == NULL)
@@ -15,18 +15,20 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		x = 0;
 		aux = ht->array[i];
 		while (aux != NULL)
 		{
 			printf("'%s': '%s'", aux->key, aux->value);
-			if (aux->next != NULL)
-				printf(", ");
 			aux = aux->next;
+			if (aux != NULL)
+				printf(", ");
+		}
+		if (i < ht->size - 1 && ht->array[i + 1] != NULL )
+		{
+			if (x == 1)
+				printf(", ");
 			x = 1;
 		}
-		if (i < ht->size - 1 && ht->array[i + 1] != NULL && x == 1)
-			printf(", ");
 	}
 	printf("}\n");
 }
