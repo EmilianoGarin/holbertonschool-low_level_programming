97dba909f489be435d2acd52edb35f65cfabae71
EmilianoGarin <emiliano.garin333@gmail.com>
2023-04-16 15:01:43 -0700

    x

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (*h == NULL)
 		return (NULL);
 
-	for (i = 0; i < idx; i++)
+	for (i = 1; i < idx; i++)
 	{
 		if (fin == NULL)
 			return (NULL);
